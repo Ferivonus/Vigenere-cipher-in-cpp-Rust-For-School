a2e6074a883df7aae1f107c476136b71b892e650
Ferivonus <ferivonus@hotmail.com>
2023-03-24 07:14:26 +0300

    v2
    v2

--- Vigenere-cipher-C++/Polyalphabetic Ciphers.cpp
+++ Vigenere-cipher-C++/Polyalphabetic Ciphers.cpp
@@ -1,93 +1,66 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
+#include <locale> // for the locale class
 using namespace std;
 
-string encrypt(string plaintext, string key) {
-    string ciphertext = "";
-    int key_len = key.length();
-    int plaintext_len = plaintext.length();
-
-    // Iterate over each character in the plaintext
-    for (int i = 0; i < plaintext_len; i++) {
-        // Get the current character and convert it to uppercase
-        char current_char = plaintext[i];
-        current_char = toupper(current_char);
-
-        // Calculate the index of the current character in the alphabet
-        int alpha_index = current_char - 'A';
-
-        // Calculate the index of the current character in the key
-        int key_index = i % key_len;
-        char key_char = key[key_index];
-        key_char = toupper(key_char);
-        int key_offset = key_char - 'A';
-
-        // Calculate the new index of the current character in the alphabet
-        int new_alpha_index = (alpha_index + key_offset) % 26;
-
-        // Calculate the new character and add it to the ciphertext
-        char new_char = new_alpha_index + 'A';
-        ciphertext += new_char;
+// Function: Encryption
+// Description: Performs Vigenere encryption and returns the result
+string Encryption(string plain_text, string key)
+{
+    string encrypted_text = "";
+    vector<int> key_number;
+    for (int i = 0; i < key.length(); i++) {
+        key_number.push_back((int)tolower(key[i]) - 97);
     }
-
-    return ciphertext;
+    int j = 0;
+    for (int i = 0; i < plain_text.length(); i++) {
+        if (isalpha(plain_text[i])) {
+            int letter = (((int)toupper(plain_text[i]) - 65) + key_number[j]) % 26;
+            if (letter < 0) letter += 26;
+            encrypted_text += (char)(letter + 65);
+            j = (j + 1) % key_number.size();
+        }
+        else {
+            encrypted_text += plain_text[i];
+        }
+    }
+    return encrypted_text;
 }
 
-string decrypt(string ciphertext, string key) {
-    string plaintext = "";
-    int key_len = key.length();
-    int ciphertext_len = ciphertext.length();
-
-    // Iterate over each character in the ciphertext
-    for (int i = 0; i < ciphertext_len; i++) {
-        // Get the current character and convert it to uppercase
-        char current_char = ciphertext[i];
-        current_char = toupper(current_char);
-
-        // Calculate the index of the current character in the alphabet
-        int alpha_index = current_char - 'A';
-
-        // Calculate the index of the current character in the key
-        int key_index = i % key_len;
-        char key_char = key[key_index];
-        key_char = toupper(key_char);
-        int key_offset = key_char - 'A';
-
-        // Calculate the new index of the current character in the alphabet
-        int new_alpha_index = (alpha_index - key_offset + 26) % 26;
-
-        // Calculate the new character and add it to the plaintext
-        char new_char = new_alpha_index + 'A';
-        plaintext += new_char;
+// Function: Decryption
+// Description: Decrypts text encrypted using Vigenere and returns the result
+string Decryption(string encrypted_text, string key)
+{
+    string plain_text = "";
+    vector<int> key_number;
+    for (int i = 0; i < key.length(); i++) {
+        key_number.push_back((int)tolower(key[i]) - 97);
     }
-
-    return plaintext;
+    int j = 0;
+    for (int i = 0; i < encrypted_text.length(); i++) {
+        if (isalpha(encrypted_text[i])) {
+            int letter = (((int)toupper(encrypted_text[i]) - 65) - key_number[j] + 26) % 26;
+            if (letter < 0) letter += 26;
+            plain_text += (char)(letter + 65);
+            j = (j + 1) % key_number.size();
+        }
+        else {
+            plain_text += encrypted_text[i];
+        }
+    }
+    return plain_text;
 }
 
 int main() {
-    //setting them in code:
-    //string plaintext = "wearediscoveredsaveyourself";
-    //string key = "deceptivedeceptivedeceptive";
-
-    //setting them with hand:
-
-    string plaintext = "";
-    string key = "";
-
-    cout << "Could u give me your plain text? uwu: ";
-    getline(cin, plaintext);
-
-    cout << "Then gimmi my key to match it uwu: ";
-    getline(cin, key);
-
-    string ciphertext = encrypt(plaintext, key);
-    string decrypted_plaintext = decrypt(ciphertext, key);
-
-    cout << "Plaintext: " << plaintext << endl;
+    setlocale(LC_ALL, "Turkish"); // set locale to support Turkish characters
+    string plain_text = "abcçdefgğhiıjklmnoöprsştuüvyz"; // the text to be encrypted
+    string key = "deceptiveabcçdefgğhiıjklmnoöprsştuüvyz"; // the key
+    string encrypted_text = Encryption(plain_text, key); // encrypt the text
+    string plain_text2 = Decryption(encrypted_text, key); // decrypt the encrypted text
+    cout << "Plain Text: " << plain_text << endl;
     cout << "Key: " << key << endl;
-    cout << "Ciphertext: " << ciphertext << endl;
-    cout << "Decrypted plaintext: " << decrypted_plaintext << endl;
-
+    cout << "Encrypted Text: " << encrypted_text << endl;
+    cout << "Decrypted Text: " << plain_text2 << endl;
     return 0;
 }
